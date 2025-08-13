2cbba4bea0eeb28563208d0e4b75a279046a37e8
ksol95 <ksol95@ya.ru>
2025-08-13 19:06:21 +0400

    fix grid

---
+++ src/js/pages/test.js
@@ -0,0 +1,9 @@
+// Импортируем основные стили
+import "../../scss/main.scss";
+// Импортируем стили, специфичные для страницы
+import "../../scss/pages/test.scss";
+// Импортируем основные JS модули
+import "../index.js";
+// Импортируйте другие необходимые JS модули
+
+console.log("Test page scripts loaded.");
